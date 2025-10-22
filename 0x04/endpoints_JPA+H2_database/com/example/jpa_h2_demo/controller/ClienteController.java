b89efc0bc55d4ea99658cbb34328cc039f6508e7
Eliane Cristina Kobayakawa <az204-elicrisko@outlook.com>
2025-10-22 16:44:36 -0300

    2. Definindo Serviços para a API REST usando Spring Data JPA + H2 Database

---
+++ 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/controller/ClienteController.java
@@ -0,0 +1,60 @@
+package com.example.jpa_h2_demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jpa_h2_demo.model.Cliente;
+import com.example.jpa_h2_demo.repository.ClienteRepository;
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("/addClient")
+    public ResponseEntity<Cliente> addClient(@RequestBody Cliente cliente) {
+        Cliente savedCliente = clienteRepository.save(cliente);
+        return new ResponseEntity<>(savedCliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/findAllClients")
+    public ResponseEntity<List<Cliente>> findAllClients() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
+
+    @GetMapping("/findClientById/{id}")
+    public ResponseEntity<Cliente> findClientById(@PathVariable("id") Long idClient) {
+        Cliente cliente = clienteRepository.findById(idClient).orElse(null);
+        if (cliente != null) {
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/removeClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerCliente(@PathVariable("id") Long idClient){
+        clienteRepository.deleteById(idClient);
+    }
+
+    @PutMapping("/updateClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCliente(@PathVariable("id") Long id, @RequestBody Cliente cliente){
+        cliente.setId(id);
+        clienteRepository.save(cliente);
+    }
+}
